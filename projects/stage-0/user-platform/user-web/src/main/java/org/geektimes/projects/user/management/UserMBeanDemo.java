5ee70b9aca6e3b81790374caa6c1b45289a670b6
linzehuan <lzh@kapark.cn>
2021-03-17 23:25:14 +0800

    feat: 集成jolokia

--- projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/management/UserMBeanDemo.java
+++ projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/management/UserMBeanDemo.java
@@ -1,15 +1,17 @@
 package org.geektimes.projects.user.management;
 
-import com.sun.jmx.mbeanserver.Introspector;
+
 import org.geektimes.projects.user.domain.User;
 
+import javax.annotation.PostConstruct;
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
 import java.lang.management.ManagementFactory;
 
 public class UserMBeanDemo {
 
-    public static void main(String[] args) throws Exception {
+    @PostConstruct
+    public void init() throws Exception{
         // 获取平台 MBean Server
         MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
         // 为 UserMXBean 定义 ObjectName
@@ -17,10 +19,10 @@ public class UserMBeanDemo {
         // 创建 UserMBean 实例
         User user = new User();
         mBeanServer.registerMBean(createUserMBean(user), objectName);
-        while (true) {
-            Thread.sleep(2000);
-            System.out.println(user);
-        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
     }
 
     private static Object createUserMBean(User user) throws Exception {
